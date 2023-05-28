820a52fc5099e3f8292b9fc1a4832f8c45c24921
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-05-28 16:17:38 +0300

    refactoring:

--- builder/app/index.js
+++ builder/app/index.js
@@ -2,27 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import parseMD from 'parse-md';
 import pug from 'pug';
-import { marked } from 'marked';
-import { markedHighlight } from "marked-highlight";
-import { mangle } from "marked-mangle";
-import { gfmHeadingId } from "marked-gfm-heading-id";
-import hljs from 'highlight.js';
-
+import {marked} from './marked.js';
 import { compile } from '@eit6609/markdown-templates';
 import { LanguageMap } from './language-map.js';
-marked.use({
-    langPrefix: ''
-})
-marked.use(markedHighlight({
-    langPrefix: 'hljs language-',
-    highlight(code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-      return hljs.highlight(code, { language }).value;
-    }
-  }));
-marked.use(mangle());
-marked.use(gfmHeadingId());
-
 
 class Templates {
     constructor(paths) {
---
+++ builder/app/marked.js
@@ -0,0 +1,21 @@
+import { marked } from 'marked';
+import { markedHighlight } from "marked-highlight";
+import { mangle } from "marked-mangle";
+import { gfmHeadingId } from "marked-gfm-heading-id";
+import hljs from 'highlight.js';
+
+
+marked.use({
+    langPrefix: ''
+})
+marked.use(markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    }
+  }));
+marked.use(mangle());
+marked.use(gfmHeadingId());
+
+export { marked };
