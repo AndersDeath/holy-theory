c40e3672eb3d041070b6117afa4a4747810698e2
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-09-04 17:48:36 +0400

    refactoring of utils.ts in builder.js

--- builder/app/utils.ts
+++ builder/app/utils.ts
@@ -1,32 +1,48 @@
 import fs from "fs";
 import path from "path";
 
-let config: any = null;
+let cachedConfig: any = null;
 
 export const getConfig = (): any => {
-  if (config) return config;
-  config = JSON.parse(fs.readFileSync("./builder/config.json", "utf-8")) as any;
-  return config;
+  if (cachedConfig !== null) {
+    return cachedConfig;
+  }
+
+  const configFilePath = path.join("./builder", "config.json");
+
+  try {
+    const configData = fs.readFileSync(configFilePath, "utf-8");
+    cachedConfig = JSON.parse(configData);
+    return cachedConfig;
+  } catch (error) {
+    throw new Error(`Error reading or parsing config.json: ${error.message}`);
+  }
 };
 
-
 export function getFiles(dir: string): string[] {
-  let results: string[] = [];
-  fs.readdirSync(dir).forEach((file) => {
-    file = dir + "/" + file;
-    const stat = fs.statSync(file);
-    if (stat && stat.isDirectory()) {
-      results = results.concat(getFiles(file));
-    } else {
-      if (path.extname(file) === ".md") {
-        results.push(file);
+  const results: string[] = [];
+
+  const readDirSync = (directory: string) => {
+    const files = fs.readdirSync(directory);
+
+    for (const file of files) {
+      const filePath = path.join(directory, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        readDirSync(filePath); // Recursively read subdirectory
+      } else if (path.extname(file) === ".md") {
+        results.push(filePath);
       }
     }
-  });
+  };
+
+  readDirSync(dir); // Start reading from the provided directory
+
   return results;
 }
 
 export const cleanContent = (content: any) => {
-  content.replace("* [Go back](../readme.md)", "");
+  content = content.replace("* [Go back](../readme.md)", "");
   return content;
-}
+};
