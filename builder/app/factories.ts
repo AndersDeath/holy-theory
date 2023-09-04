a56ba888d9e2f9e1388f0b3975c67a6fcb795534
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-09-04 17:59:52 +0400

    create builder factory

--- builder/app/factories.ts
+++ builder/app/factories.ts
@@ -3,121 +3,136 @@ import { Templates } from "./templates";
 import { getConfig, getFiles } from "./utils";
 import { folders } from "./constants";
 import path from "path";
-import { marked } from "./marked"; 
+import { marked } from "./marked";
 
-const baseUrl = "/builder/test/";
+export const createBuilder = () => {
+  const paths = getConfig().templates;
 
-const basePath = "." + baseUrl;
-const paths = getConfig().templates;
+  const baseUrl = "/builder/test/";
 
-const templates = new Templates(paths, "index");
+  const basePath = "." + baseUrl;
+  const templates = new Templates(paths, "index");
 
-export const buildTableOfContents = (nav, content) => {
-  fs.writeFileSync(
-    basePath + "/table-of-contents.html",
-    templates.getData()["table-of-contents"].build({
-      navigation: nav,
-      values: content,
-    })
-  );
-};
+  const buildTableOfContents = (nav, content) => {
+    fs.writeFileSync(
+      basePath + "/table-of-contents.html",
+      templates.getData()["table-of-contents"].build({
+        navigation: nav,
+        values: content,
+      })
+    );
+  };
 
-export const buildLanguagesHtml = (nav, lm) => {
-  fs.writeFileSync(
-    basePath + "/languages.html",
-    templates.getData()["languages"].build({
-      navigation: nav,
-      values: Object.fromEntries(lm.get()),
-    })
-  );
-};
-export const buildAllHtml = (nav, articles) => {
-  fs.writeFileSync(
-    basePath + "/all.html",
-    templates.getData()["layout"].build({
-      header: "Holy Theory",
-      values: articles,
-      navigation: nav,
-    })
-  );
-};
+  const buildLanguagesHtml = (nav, lm) => {
+    fs.writeFileSync(
+      basePath + "/languages.html",
+      templates.getData()["languages"].build({
+        navigation: nav,
+        values: Object.fromEntries(lm.get()),
+      })
+    );
+  };
 
-export const buildIndexHtml = (nav) => {
-  fs.writeFileSync(
-    basePath + "/index.html",
-    templates.getData()["main"].build({ navigation: nav })
-  );
-};
+  const buildAllHtml = (nav, articles) => {
+    fs.writeFileSync(
+      basePath + "/all.html",
+      templates.getData()["layout"].build({
+        header: "Holy Theory",
+        values: articles,
+        navigation: nav,
+      })
+    );
+  };
 
-export const buildArticleHtml = (nav, item) => {
-  fs.writeFileSync(
-    basePath + item.meta.category + "/" + item.meta.fileName.dashed + ".html",
-    templates.getData()["article"].build({
-      navigation: nav,
-      title: item.title,
-      body: item.body,
-    })
-  );
-};
+  const buildIndexHtml = (nav) => {
+    fs.writeFileSync(
+      basePath + "/index.html",
+      templates.getData()["main"].build({ navigation: nav })
+    );
+  };
 
-export const buildArticleMdHtml = (item) => {
-  if (!fs.existsSync(basePath + "/md")) {
-    fs.mkdirSync(basePath + "/md", { recursive: true });
-  }
-  fs.writeFileSync(
-    `./builder/test/md/${item.title.replace("/", "-")}.md`,
-    templates.getData()["articleMD"].build(item)
-  );
-};
+  const buildArticleHtml = (nav, item) => {
+    fs.writeFileSync(
+      basePath + item.meta.category + "/" + item.meta.fileName.dashed + ".html",
+      templates.getData()["article"].build({
+        navigation: nav,
+        title: item.title,
+        body: item.body,
+      })
+    );
+  };
 
-export const buildFoldersForCategories = (basePath, item) => {
-  if (!fs.existsSync(basePath + item.meta.category)) {
-    fs.mkdirSync(basePath + item.meta.category, { recursive: true });
-  }
-};
+  const buildArticleMdHtml = (item) => {
+    if (!fs.existsSync(basePath + "/md")) {
+      fs.mkdirSync(basePath + "/md", { recursive: true });
+    }
+    fs.writeFileSync(
+      `./builder/test/md/${item.title.replace("/", "-")}.md`,
+      templates.getData()["articleMD"].build(item)
+    );
+  };
 
-export const buildNavigation = (templates) => {
-  return templates.getData()["nav"].build({
-    values: [
-      {
-        href: "/holy-theory" + baseUrl,
-        title: "Main page",
+  const buildFoldersForCategories = (basePath, item) => {
+    if (!fs.existsSync(basePath + item.meta.category)) {
+      fs.mkdirSync(basePath + item.meta.category, { recursive: true });
+    }
+  };
+
+  const buildNavigation = () => {
+    return templates.getData()["nav"].build({
+      values: [
+        {
+          href: "/holy-theory" + baseUrl,
+          title: "Main page",
+        },
+        {
+          href: "/holy-theory" + baseUrl + "/languages.html",
+          title: "Statistics",
+        },
+      ],
+    });
+  };
+
+  const getData = () => {
+    let data: string[][] = [];
+    folders.forEach((e: any) => {
+      data.push(getFiles(path.join("./", e)));
+    });
+    return data;
+  };
+
+  const buildDataItem = (input: any) => {
+    return {
+      title: input.metadata.title,
+      body: marked.parse(input.cleanedContent),
+      bodyMD: input.cleanedContent,
+
+      meta: {
+        category: input.pathObj.dir,
+        url:
+          "/holy-theory" +
+          baseUrl +
+          input.pathObj.dir +
+          "/" +
+          input.pathObj.name.replace(" ", "-").toLowerCase() +
+          ".html",
+        fileName: {
+          original: input.pathObj.name,
+          dashed: input.pathObj.name.replace(" ", "-").toLowerCase(),
+        },
       },
-      {
-        href: "/holy-theory" + baseUrl + "/languages.html",
-        title: "Statistics",
-      },
-    ],
-  });
-};
-
-export const getData = () => {
-  let data: string[][] = [];
-  folders.forEach((e: any) => {
-    data.push(getFiles(path.join("./", e)));
-  });
-  return data;
-};
-
-export const buildDataItem = (input: any) => {
+    };
+  };
   return {
-    title: input.metadata.title,
-    body: marked.parse(input.cleanedContent),
-    bodyMD: input.cleanedContent,
-
-    meta: {
-      category: input.pathObj.dir,
-      url:
-        "/holy-theory" +
-        baseUrl +
-        input.pathObj.dir +
-        "/" +
-        input.pathObj.name.replace(" ", "-").toLowerCase() +
-        ".html",
-      fileName: {
-        original: input.pathObj.name,
-        dashed: input.pathObj.name.replace(" ", "-").toLowerCase(),
-      },
-    },
+    buildTableOfContents,
+    buildLanguagesHtml,
+    buildAllHtml,
+    buildIndexHtml,
+    buildArticleHtml,
+    buildArticleMdHtml,
+    buildFoldersForCategories,
+    buildNavigation,
+    getData,
+    buildDataItem,
   };
 };
