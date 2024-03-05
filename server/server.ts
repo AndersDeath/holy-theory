b3a0a7a33d0ae0f9de811c893178a5179a293501
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-03-05 19:24:13 +0100

    update server

--- server/server.ts
+++ server/server.ts
@@ -1,30 +1,31 @@
 import express, { Express, Request, Response } from "express";
-import {exec, execFile} from 'child_process';
-import fs from 'fs-extra';
+import { exec, execFile, spawn } from "child_process";
+import fs from "fs-extra";
 
 const app: Express = express();
 const port = 3000;
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
 app.get("/builder", (req: Request, res: Response) => {
-  const page = fs.readFileSync('./server/templates/builder.html','utf-8');
+  const page = fs.readFileSync("./server/templates/builder.html", "utf-8");
   res.send(page);
 });
 
 app.get("/builder/run", (req: Request, res: Response) => {
-  console.log('the builder is run');
+  console.log("the builder is run");
+
+
   try {
-    execFile(`./scripts/generate_all.sh`, [], (error: any, stdout: any, stderr: any) => {
-      console.log('exec works');
-      console.log(error);
-      console.log(stdout);
-      res.redirect('/builder');
-   });
-  } catch(e) {
+    const process = spawn(`./scripts/generate_all.sh`, []);
+
+    process.stdout.on("data", (ress: any) => {
+      console.log("Log: " + ress);
+      res.redirect("/builder");
+    });
+  } catch (e) {
     console.log(e);
   }
- 
 });
 
 app.listen(port, () => {
