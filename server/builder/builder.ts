4fb2039492894a101dfea5b4fa7b5791246659b5
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-04-18 19:21:11 +0200

    update

--- server/builder/builder.ts
+++ server/builder/builder.ts
@@ -1,14 +1,30 @@
+import * as fs from "fs-extra";
+import * as path from "path";
+
 export class Builder {
+  config = {
+    outputFolder: "",
+  };
   constructor(config: any) {
     console.log("Builder started");
+    this.config = config;
   }
 
   run() {
-    this.parseMDInit(this.parseFolder);
+    this.parseMDInit((parseMD) => {
+      this.init(parseMD, "./");
+    });
   }
 
-  parseFolder(parseMD: any) {
+  async init(parseMD: any, rootFolder: string) {
     console.log(parseMD);
+    const folders = await fs.readdir(rootFolder);
+    for (const folder of folders) {
+      const folderPath = path.join(rootFolder, folder);
+      if (fs.statSync(folderPath).isDirectory()) {
+        await this.parseFolder(folder, folderPath);
+      }
+    }
   }
 
   parseMDInit(callback: (parseMD) => void) {
@@ -16,5 +32,30 @@ export class Builder {
       const parseMD = module.default;
       callback(parseMD);
     });
+  }
+
+  async parseFolder(folder: any, folderPath: any) {
+    const sectionName = folder.replace(/ /g, "-");
+    const sectionOutputFolder = path.join(
+      this.config.outputFolder,
+      sectionName
+    );
+    await fs.mkdirp(sectionOutputFolder);
+
+    const files = await fs.readdir(folderPath);
+
+    // for (const file of files) {
+    //   await parseFile(file, folderPath, sectionOutputFolder, sectionName);
+    // }
+
+    // await fs.writeFile(
+    //   path.join(sectionOutputFolder, "index." + type),
+    //   await buildLinksList(
+    //     allContentWithSections.filter(
+    //       (e: ContentEntity) => e.section === sectionName
+    //     ),
+    //     type
+    //   )
+    // );
   }
 }
