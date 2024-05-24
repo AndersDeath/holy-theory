04fb295b8f4e237b1cbbb1d6f63b7a796988ab60
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-05-24 18:07:06 +0200

    add logger page to server

--- server/server.ts
+++ server/server.ts
@@ -14,12 +14,24 @@ app.get("/builder", (req: Request, res: Response) => {
 });
 
 app.get("/builder/logs", (req: Request, res: Response) => {
-  const page = fs.readFileSync("./builder3-logs.log", "utf-8");
-  res.send(page);
-})
+  const page = fs.readFileSync("./server/templates/logs.html", "utf-8");
+  const logFile = fs.readFileSync("./builder3-logs.log", "utf-8");
+  let logs = "<div>";
+  logFile.split("\n").forEach((element) => {
+    logs += element + "</br>";
+  });
+  logs += "</div>";
+
+  const replaceKey = /put_logs_here/g;
+  const output = page.replace(replaceKey, logs);
+  res.send(output);
+});
 
 app.get("/builder/run", (req: Request, res: Response) => {
-  const page = fs.readFileSync("./server/templates/generation-run.html", "utf-8");
+  const page = fs.readFileSync(
+    "./server/templates/generation-run.html",
+    "utf-8"
+  );
   const logger = new Logger();
   logger.log("The Builder Runner has started!");
   logger.time("Builder working timer");
