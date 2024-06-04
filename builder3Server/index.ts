c2f5b51d345fad1346a52fca13cbad83614a999e
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-06-04 19:16:41 +0200

    server refactoring

--- builder3Server/index.ts
+++ builder3Server/index.ts
@@ -1,85 +1,10 @@
-import express, { Express, Request, Response } from "express";
-import { spawn } from "child_process";
-import fs from "fs-extra";
-import { Builder3 } from "./builder/builder";
-import { Logger } from "./builder/logger/logger";
+import express, { Express } from "express";
+import { routesInit } from "./server/bootstrap";
 
 const app: Express = express();
 const port = 3000;
 
-const B3 = new Builder3({
-  sourceRootPath: "./content",
-  htmlOutputPath: "./static2",
-  markdownOutputPath: "./markdown2",
-  tempFolderPath: "./temp",
-  imageFolderPath: "./images",
-});
-
-app.use("/static", express.static("static"));
-
-app.get("/", (req: Request, res: Response) => {
-  const page = fs.readFileSync("./builder3Server/templates/builder.html", "utf-8");
-  res.send(page);
-});
-
-app.get("/builder/logs", (req: Request, res: Response) => {
-  const page = fs.readFileSync("./builder3Server/templates/logs.html", "utf-8");
-  const logFile = fs.readFileSync("./builder3-logs.log", "utf-8");
-  let logs = "<div>";
-  logFile.split("\n").forEach((element) => {
-    logs += element + "</br>";
-  });
-  logs += "</div>";
-
-  const replaceKey = /put_logs_here/g;
-  const output = page.replace(replaceKey, logs);
-  res.send(output);
-});
-
-app.get("/builder/logs/clear", async (req: Request, res: Response) => {
-  const logger = new Logger();
-  await logger.clearLogs();
-  res.redirect("/builder/logs");
-});
-
-app.get("/builder/run", (req: Request, res: Response) => {
-  let targets = [];
-  let categories = [];
-  if (req.query.targets) {
-    targets = req.query.targets.toString().split(",");
-  }
-  if (req.query.categories) {
-    targets = req.query.categories.toString().split(",");
-  }
-
-  const page = fs.readFileSync(
-    "./builder3Server/templates/generation-run.html",
-    "utf-8"
-  );
-  const logger = new Logger();
-  logger.log("The Builder Runner has started!");
-  logger.time("Builder working timer");
-
-  B3.run({
-    targets,
-    bookSettings: {
-      categories,
-    },
-  }).then(() => {
-    logger.log("The work of script finished");
-    logger.timeEnd("Builder working timer");
-  });
-
-  res.send(page);
-});
-
-app.get("/clear-everything", async (req: Request, res: Response) => {
-  await fs.remove("./static2");
-  await fs.remove("./markdown2");
-  await fs.remove("./temp");
-  await fs.remove("./builder3-logs.log");
-  res.redirect("/");
-});
+routesInit(app);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ builder3Server/server/bootstrap.ts
@@ -0,0 +1,83 @@
+import express, { Express, Request, Response } from "express";
+import fs from "fs-extra";
+
+import { Builder3 } from "../builder/builder";
+import { Logger } from "../builder/logger/logger";
+
+export const routesInit = (app: any) => {
+
+    const B3 = new Builder3({
+        sourceRootPath: "./content",
+        htmlOutputPath: "./static2",
+        markdownOutputPath: "./markdown2",
+        tempFolderPath: "./temp",
+        imageFolderPath: "./images",
+    });
+
+    app.use("/static", express.static("static"));
+
+    app.get("/", (req: Request, res: Response) => {
+        const page = fs.readFileSync("./builder3Server/templates/builder.html", "utf-8");
+        res.send(page);
+    });
+
+    app.get("/builder/logs", (req: Request, res: Response) => {
+        const page = fs.readFileSync("./builder3Server/templates/logs.html", "utf-8");
+        const logFile = fs.readFileSync("./builder3-logs.log", "utf-8");
+        let logs = "<div>";
+        logFile.split("\n").forEach((element) => {
+            logs += element + "</br>";
+        });
+        logs += "</div>";
+
+        const replaceKey = /put_logs_here/g;
+        const output = page.replace(replaceKey, logs);
+        res.send(output);
+    });
+
+    app.get("/builder/logs/clear", async (req: Request, res: Response) => {
+        const logger = new Logger();
+        await logger.clearLogs();
+        res.redirect("/builder/logs");
+    });
+
+    app.get("/builder/run", (req: Request, res: Response) => {
+        let targets = [];
+        let categories = [];
+        if (req.query.targets) {
+            targets = req.query.targets.toString().split(",");
+        }
+        if (req.query.categories) {
+            targets = req.query.categories.toString().split(",");
+        }
+
+        const page = fs.readFileSync(
+            "./builder3Server/templates/generation-run.html",
+            "utf-8"
+        );
+        const logger = new Logger();
+        logger.log("The Builder Runner has started!");
+        logger.time("Builder working timer");
+
+        B3.run({
+            targets,
+            bookSettings: {
+                categories,
+            },
+        }).then(() => {
+            logger.log("The work of script finished");
+            logger.timeEnd("Builder working timer");
+        });
+
+        res.send(page);
+    });
+
+    app.get("/clear-everything", async (req: Request, res: Response) => {
+        await fs.remove("./static2");
+        await fs.remove("./markdown2");
+        await fs.remove("./temp");
+        await fs.remove("./builder3-logs.log");
+        res.redirect("/");
+    });
+
+};
