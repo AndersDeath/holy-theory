2f120629fd4c5b2a5357852981b3711fc4d23662
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-06-16 13:41:11 +0200

    style(server): remove commented code

--- builder3Server/server/bootstrap.ts
+++ builder3Server/server/bootstrap.ts
@@ -1,9 +1,4 @@
 import express, { Request, Response } from "express";
-import { indexController } from "./controllers/index.controller";
-import { builderLogsController } from "./controllers/builderLogs.controller";
-import { builderLogsClearController } from "./controllers/builderLogsClear.controller";
-import { builderRunController } from "./controllers/builderRun.controller";
-import { clearEverythingController } from "./controllers/clearEverything.controller";
 import { Controllers } from "./controllers";
 
 export const routesInit = (app: any): void => {
@@ -17,25 +12,4 @@ export const routesInit = (app: any): void => {
       });
     }
   });
-  //
-  // app.get(indexController.route, (req: Request, res: Response): void => {
-  //   indexController.controller(req, res);
-  // });
-  //
-  // app.get(builderLogsController.route, (req: Request, res: Response): void => {
-  //   builderLogsController.controller(req, res);
-  // });
-  //
-  // app.get(builderLogsClearController.route, async (req: Request, res: Response): Promise<void> => {
-  //   await builderLogsClearController.controller(req, res);
-  // });
-  //
-  // app.get(builderRunController.route, (req: Request, res: Response): void => {
-  //   builderRunController.controller(req, res);
-  // });
-  //
-  // app.get(clearEverythingController.route, async (req: Request, res: Response): Promise<void> => {
-  //   await clearEverythingController.controller(req, res);
-  // });
-
 };
